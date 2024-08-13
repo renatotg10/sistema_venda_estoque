8e65518823e7a7596721bc175a3743484ad9923e
Renato Gomes <renatotg10@gmail.com>
2024-08-13 15:17:55 -0300

    Configurado o ajuste tamanho campo exibicao relatorio

--- relatorios.py
+++ relatorios.py
@@ -39,7 +39,7 @@ class GeracaoRelatorios(tk.Frame):
         self.button_relatorio_compras = tk.Button(self, text="Relatório de Compras", command=self.gerar_relatorio_compras)
         self.button_relatorio_compras.pack(pady=5)
 
-        self.text_relatorio = tk.Text(self)
+        self.text_relatorio = tk.Text(self, width=120, height=20)
         self.text_relatorio.pack(pady=10)
 
     def gerar_relatorio_estoque(self):
